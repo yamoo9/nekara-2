4d6f28a311c828ab7223b58ff4cf3a338dad55d9
yamoo9 <yamoog@naver.com>
2021-11-25 13:48:41 +0900

    [수정] 크로스 플랫폼 환경변수 설정

--- imperative-programming/server/index.js
+++ imperative-programming/server/index.js
@@ -1,8 +1,8 @@
 const liveServer = require('live-server');
-const { PORT || OPEN } = process.env;
+const { PORT, OPEN } = process.env;
 
 liveServer.start({
   host: 'localhost',
   port: PORT || 3000,
-  open: OPEN || false,
+  open: `/${OPEN}` || false,
 });
