6509a69857c95a8267d7f31062fd60753e8ede39
yamoo9 <yamoog@naver.com>
2021-11-27 08:24:08 +0900

    [수정] ESLint 글로벌 설정

--- preparing-for-react/.eslintrc.cjs
+++ preparing-for-react/.eslintrc.cjs
@@ -3,7 +3,13 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
-    'globals/jest': true,
+  },
+  // [ESLint 글로벌 설정]
+  //  ESLint는 실행 환경에 존재하는 전역 변수에 대해 가정하지 않습니다.
+  //  사용 가능한 전역 변수에 대한 지식이 필요한 규칙을 사용하려면 구성 파일에서 전역 변수를 정의하거나 소스 코드의 구성 주석을 사용하면 됩니다.
+  //  참고: https://eslint.org/docs/user-guide/configuring/language-options#specifying-globals
+  globals: {
+    jest: true,
   },
   extends: [
     'eslint:recommended',
