048e7996cdc05f76c8595cef4993d33486c3deeb
yamoo9 <yamoog@naver.com>
2021-11-26 12:54:19 +0900

    [학습] 테스트 라이브러리 작성
    - throwError 유틸리티
    - describe 유틸리티
    - test 유틸리티
    - expect 유틸리티

---
+++ preparing-for-react/client/src/utils/tests.js
@@ -0,0 +1,61 @@
+import throwError from './throwError.js';
+
+// 기대(expect) 값을 검토하는 유틸리티
+// 코드 사용 예시)
+// expect(전달값).toBe(기대값)
+// expect(전달값).not.toBe(기대값)
+// expect(전달값).toBeInTheDocument(기대값)
+// expect(전달값).not.toBeInTheDocument(기대값)
+
+function expect(received) {
+  // 전달 값과 비교할 수 있는 유틸리티 모음 객체 반환
+  return {
+    toBe(expected) {
+      // 전달값과 기대값을 비교해서 같지 않으면 오류 발생
+      if (received !== expected) {
+        throwError(`${received}와 ${expected} 값은 동일하지 않습니다.`);
+      }
+    },
+    toBeInTheDocoument() {
+      if (!document.body.contains(received)) {
+        throwError(`${received}는 문서에 포함되지 않습니다.`);
+      }
+    },
+    not: {
+      toBe(expected) {
+        if (received === expected) {
+          throwError(`${received}와 ${expected} 값이 동일합니다.`);
+        }
+      },
+      toBeInTheDocoument() {
+        if (document.body.contains(received)) {
+          throwError(`${received}는 문서에 포함되어 있습니다.`);
+        }
+      },
+    }
+  }
+}
+
+// 테스트(test) 유틸리티
+// 코드 사용 예시)
+// test('1 + 1 = 2', () => expect(1 + 1).not.toBe('12'))
+function test(description, callback) {
+  // 오류 발생 여부 감지
+  try {
+    callback();
+    console.log(`🟢 테스트 성공: ${description}`);
+  } catch(error) {
+    console.groupCollapsed(`🔴 테스트 실패: ${description}`);
+    console.error(error.message);
+    console.groupEnd();
+  }
+}
+
+// 기술(describe) 유틸리티
+// 코드 사용 예시)
+// describe('테스트 리스트 항목을 대변하는 레이블', () => { test(); test(); ... })
+function describe(testLabel, callback) {
+  console.group(testLabel);
+  callback();
+  console.groupEnd();
+}
