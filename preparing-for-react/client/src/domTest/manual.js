ec9535807831111bfee13b653802d311f86f8359
yamoo9 <yamoog@naver.com>
2021-11-28 08:24:30 +0900

    [수정] domTest
    - index.js → manual.js
    - main.js 예시 코드 제거

--- preparing-for-react/client/src/domTest/manual.js
+++ preparing-for-react/client/src/domTest/manual.js
@@ -1,30 +1,23 @@
 import { describe, test, expect } from '../utils/index.js';
 
 const appNode = document.getElementById('app');
+const headlineNode = appNode.querySelector('h1,h2,h3,h4,h5,h6');
 
 describe('DOM 테스트', () => {
-  // 문서의 제목이 "React 앱 개발환경 구성" 인가?
   test('문서의 제목이 "React 앱 개발환경 구성" 인가?', () => {
     expect(document.title).toBe('React 앱 개발환경 구성');
   });
-  // 문서에 #app 요소가 존재하는가?
   test('문서에 #app 요소가 존재하는가?', () => {
     expect(appNode).toBeInTheDocoument();
   });
-  // #app 요소 안에 제목 요소가 포함되어 있나?
   test('#app 요소 안에 제목 요소가 포함되어 있나?', () => {
     const hasHeadlineElement = !!appNode.querySelector('h1,h2,h3,h4,h5,h6');
     expect(hasHeadlineElement).toBeTruthy();
   });
-  // 제목의 내용은 "React 앱 개발"
   test('제목의 내용은 "React 앱 개발"', () => {
-    const headlineNode = appNode.querySelector('h1,h2,h3,h4,h5,h6');
     expect(headlineNode.textContent).toBe('React 앱 개발');
   });
-  // 제목 요소는 headline 클래스를 포함하는가?
   test('제목 요소는 headline 클래스를 포함하는가?', () => {
-    const headlineNode = appNode.querySelector('h1,h2,h3,h4,h5,h6');
-    // expect(headlineNode.classList.contains('headline')).toBeTruthy();
     expect(headlineNode).toHaveClass('headline');
   });
 });
