7ded71f0eee533934573e744c5bfb749b5dc7538
yamoo9 <yamoog@naver.com>
2021-12-13 15:24:19 +0900

    [미션] Flexbox 컴포넌트

---
+++ presentation-component/src/components/Flexbox/Flexbox.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Flexbox } from './flexbox';
+
+<Flexbox.Container
+  // direction="column"
+  // wrap="wrap"
+  flow="column wrap"
+  justify="center"
+  items="center"
+  content="space-evenly"
+  // gap={10}
+  gap={{
+    row: 10,
+    column: 10,
+  }}
+>
+  <Flexbox.Item
+    order={-1}
+    // grow={1}
+    // shrink={0}
+    // basis="30%"
+    flex="1 0 30%"
+    self="flex-end"
+  ></Flexbox.Item>
+</Flexbox.Container>;
