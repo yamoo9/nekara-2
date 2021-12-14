fbd04c154d027f8956236331cdd90726fc3db949
yamoo9 <yamoog@naver.com>
2021-12-14 17:00:54 +0900

    [학습] 라이프 사이클 메서드
    - componentDidMount
    - componentDidUpdate

--- container-component/src/components/AppHeader/AppHeader.js
+++ container-component/src/components/AppHeader/AppHeader.js
@@ -1,47 +1,81 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { ReactComponent as ReactLogo } from '../../assets/logo.svg';
 
 // stateful component
-export class AppHeader extends React.Component {
-  constructor(props) {
-    super(props);
-    // 컴포넌트 상태 설정
-    this.state = {
-      brand: {
-        label: 'React',
-        className: 'App-logo',
-      },
-      descripition: '',
-      learnLink: {
-        className: 'App-link',
-        href: 'https://reactjs.org',
-        text: 'Learn React',
-        isExternal: true
-      }
-    };
+export class AppHeader extends Component {
+  // class field [new syntax]
+  // 컴포넌트 상태 설정
+  state = {
+    brand: {
+      label: 'React',
+      className: 'App-logo',
+    },
+    learnLink: {
+      className: 'App-link',
+      href: 'https://reactjs.org',
+      text: 'Learn React',
+      isExternal: true,
+    },
+  };
+
+  // 컴포넌트의 메서드 (사용자 정의)
+  getDescription() {
+    return [
+      'Edit',
+      ' ',
+      <code key="appEntryFile">src/App.js</code>,
+      ' ',
+      'and save to reload.',
+    ];
   }
 
+  // 컴포넌트 메서드 (React 빌트인)
+  // 선언형 프로그래밍
   render() {
+    console.log('render', document.querySelector('.App-header'))
+
     const {
       brand: { label, className },
-      learnLink
+      learnLink,
     } = this.state;
 
     return (
       <header className="App-header">
         <ReactLogo title={label} className={className} />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{this.getDescription()}</p>
         <a
           className={learnLink.className}
           href={learnLink.href}
-          target={learnLink.isExternal && "_blank"}
-          rel={learnLink.isExternal && "noopener noreferrer"}
+          target={learnLink.isExternal && '_blank'}
+          rel={learnLink.isExternal && 'noopener noreferrer'}
         >
           {learnLink.text}
         </a>
       </header>
     );
   }
+
+  // 라이프 사이클 메서드
+  // 명령형 프로그래밍
+  // 컴포넌트가 실제 DOM에 마운트 된 이후 실행
+  componentDidMount() {
+    console.log('컴포넌트가 실제 DOM에 마운트 된 이후 실행');
+    // DOM 노드에 접근 가능 여부
+    console.log('componentDidMount', document.querySelector('.App-header'))
+
+    // 컴포넌트의 상태 업데이트 (시간의 흐름에 따라 제어)
+    setTimeout(() => {
+      this.setState({
+        learnLink: {
+          ...this.state.learnLink,
+          text: '리엑트 함께 배워요!! 😃'
+        }
+      })
+    }, 3000);
+  }
+
+  componentDidUpdate() {
+    console.log('컴포넌트가 업데이트 되었습니다!');
+  }
+  
 }
--- container-component/src/index.js
+++ container-component/src/index.js
@@ -4,9 +4,6 @@ import { render } from 'react-dom';
 import App from './app/App';
 import { AppHeader } from './components'
 
-// static method (sync)
-// import reportWebVitals from './reportWebVitals';
-
 render(
   <StrictMode>
     <App>
@@ -16,14 +13,8 @@ render(
   document.getElementById('root')
 );
 
-// React 앱은 Node.js 환경에서 컴파일 되므로
-// process.env를 사용할 수 있다.
 const { NODE_ENV: devOrProdMode } = process.env;
 
-// dynamic import (async)
-// 어디서나 호출 가능
-// reportWebVitals();
-// 빌드 할 때만, reportWebVitals 모듈 동적 호출
 if (devOrProdMode.includes('production')) {
   
   // Promise 방식
