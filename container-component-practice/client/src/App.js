18df9d7c9c0ab601c3baa5dd7fabc6b60f991363
yamoo9 <yamoog@naver.com>
2021-12-20 16:41:58 +0900

    [학습] React 이벤트 해들링 (with props 드릴링)

--- container-component-practice/client/src/App.js
+++ container-component-practice/client/src/App.js
@@ -1,10 +1,49 @@
 import React from 'react';
 import { Dialog } from 'components';
 
+function Box({ isVisible, onClose }) {
+  return (
+    <div className="box" style={{ marginTop: 30, paddingBottom: 60 }}>
+      <Dialog isVisible={isVisible} onClose={onClose} />
+      <p>
+        야무의, <a href="#">한글 로렘입숨</a>. 나무다리 흐렸다., 등불 섬들은
+        몸을 구름처럼 어두워지는 조차 통하는 K는 큰 보이지 떠올랐는지 섬마다
+        부풀어 도착했다. 풀고 바다에 눈에 듯싶었다. 마을은. 풀고 눈에, 허공을
+        성이 K는 피었다. 따라서.
+      </p>
+      <p>
+        마을은 하늘에 도착했다. 그뭄달이 서서, 따라서, 저녁 결박된 피었다.
+        비치어, 어두워지는 길에서 꽃피는 성이 버려진 파묻혀. 보이지, 안개와
+        수평선 구름처럼, 띄지 성이, <a href="#">등불</a> 떠올랐는지 어두워지는
+        조차 바다에 풀고 않을뿐더러! 조차.
+      </p>
+      <p>
+        결박된 깊이 희멀건 언제 파묻혀. 마을은 수평선 따라서 흘러가는 하늘에
+        깊이 그뭄달빛은 조금도 어둠에 성이 있었다. 희미한 바다에 있는 않을뿐더러
+        도착했다. 너머로 등불, 사슬을 꽃피는 안개와 큰, 섬들은 걸려 결박된!
+      </p>
+    </div>
+  );
+}
+
 class App extends React.Component {
   state = {
     isVisibleDialog: false,
   };
+
+  // 상태 업데이트 메서드(클래스의 인스턴스가 소유한 함수)
+  handleShowDialog() {
+    console.log('show dialog');
+    this.setState({
+      isVisibleDialog: true,
+    });
+  }
+
+  handleHideDialog() {
+    this.setState({
+      isVisibleDialog: false,
+    });
+  }
 
   render() {
     return (
@@ -16,54 +55,27 @@ class App extends React.Component {
       >
         <button
           type="button"
-          class="openDialogButton"
+          className="openDialogButton"
           aria-haspopup="dialog"
           aria-label="모달 다이얼로그 열기"
           title="모달 다이얼로그 열기"
-          onClick={() => {
-            this.setState({
-              isVisibleDialog: !this.state.isVisibleDialog,
-            });
-          }}
+          onClick={this.handleShowDialog.bind(this)}
         >
           <svg
             width="24"
             height="24"
             xmlns="http://www.w3.org/2000/svg"
-            fill-rule="evenodd"
-            clip-rule="evenodd"
+            fillRule="evenodd"
+            clipRule="evenodd"
           >
             <path d="M14 4h-13v18h20v-11h1v12h-22v-20h14v1zm10 5h-1v-6.293l-11.646 11.647-.708-.708 11.647-11.646h-6.293v-1h8v8z" />
           </svg>
         </button>
 
-        <Dialog isVisible={this.state.isVisibleDialog} />
-
-        <div
-          className="box"
-          style={{
-            height: 280,
-          }}
-        >
-          <p>
-            야무의, <a href="#">한글 로렘입숨</a>. 나무다리 흐렸다., 등불 섬들은
-            몸을 구름처럼 어두워지는 조차 통하는 K는 큰 보이지 떠올랐는지 섬마다
-            부풀어 도착했다. 풀고 바다에 눈에 듯싶었다. 마을은. 풀고 눈에,
-            허공을 성이 K는 피었다. 따라서.
-          </p>
-          <p>
-            마을은 하늘에 도착했다. 그뭄달이 서서, 따라서, 저녁 결박된 피었다.
-            비치어, 어두워지는 길에서 꽃피는 성이 버려진 파묻혀. 보이지, 안개와
-            수평선 구름처럼, 띄지 성이, <a href="#">등불</a> 떠올랐는지
-            어두워지는 조차 바다에 풀고 않을뿐더러! 조차.
-          </p>
-          <p>
-            결박된 깊이 희멀건 언제 파묻혀. 마을은 수평선 따라서 흘러가는 하늘에
-            깊이 그뭄달빛은 조금도 어둠에 성이 있었다. 희미한 바다에 있는
-            않을뿐더러 도착했다. 너머로 등불, 사슬을 꽃피는 안개와 큰, 섬들은
-            걸려 결박된!
-          </p>
-        </div>
+        <Box
+          isVisible={this.state.isVisibleDialog}
+          onClose={this.handleHideDialog.bind(this)}
+        />
       </div>
     );
   }
--- container-component-practice/client/src/components/Dialog/Dialog.js
+++ container-component-practice/client/src/components/Dialog/Dialog.js
@@ -1,36 +1,49 @@
 import './Dialog.css';
 import React from 'react';
+import { createPortal } from 'react-dom';
 
-export const Dialog = ({ isVisible }) => {
-  return isVisible ? (
+// 모달 다이얼로그 컴포넌트가 차원이동(portal) 렌더링 될 컨테이너 요소(실제 DOM 노드)
+// const dialogContainer = document.getElementById('modal-dialog-zone');
+
+function renderDialog(isVisible, onClose) {
+  return (
     <div
       role="dialog"
       aria-modal="true"
       className="modalDialog"
-      aria-hidden="true"
+      aria-hidden={!isVisible}
       aria-label="React Portal﹕모달 다이얼로그"
     >
       <div className="content">
         <h2>포털</h2>
-        <p>여기가 React 앱 밖의 세상인가요?!</p>
+        <p>
+          여기가 <a href="#">React 앱 밖의 세상</a>인가요?!
+        </p>
       </div>
       <button
         type="button"
         className="closeDialogButton"
         aria-label="모달 다이얼로그 닫기"
         title="모달 다이얼로그 닫기"
+        onClick={onClose}
       >
         <svg
           width="24"
           height="24"
           xmlns="http://www.w3.org/2000/svg"
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
         >
           <path d="M12 11.293l10.293-10.293.707.707-10.293 10.293 10.293 10.293-.707.707-10.293-10.293-10.293 10.293-.707-.707 10.293-10.293-10.293-10.293.707-.707 10.293 10.293z" />
         </svg>
       </button>
       <div role="presentation" className="dim" />
     </div>
-  ) : null;
+  );
+}
+
+export const Dialog = ({ isVisible, onClose }) => {
+  return isVisible
+    ? createPortal(renderDialog(isVisible, onClose), document.body)
+    : null;
 };
