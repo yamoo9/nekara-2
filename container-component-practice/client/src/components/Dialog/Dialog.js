c79e5ad67cb304afc2eaf4a850f02054edbd550b
yamoo9 <yamoog@naver.com>
2021-12-23 17:17:56 +0900

    [학습] class → function + hooks 리팩터링

--- container-component-practice/client/src/components/Dialog/Dialog.js
+++ container-component-practice/client/src/components/Dialog/Dialog.js
@@ -1,115 +1,51 @@
 import styles from './Dialog.module.css';
-import React from 'react';
+import { useRef, useEffect, useCallback } from 'react';
+import { string, elementType, any, func, oneOfType } from 'prop-types';
 import { createPortal } from 'react-dom';
 import { getTabbableElements, classNames } from 'utils';
 
+// React 컴포넌트 - elementType (e.g: Button)
+// React 요소 - element (e.g: <Button/>, React.createElement(Button))
+
 // [미션]
 // 클래스 컴포넌트가 아닌, 함수 컴포넌트 + React 훅을 사용해 동일하게 작동되도록 구성
-// 컴파운드 컴포넌트 → 함수_컴포넌트.컴포넌트 작성
-// props 검사 → PropTypes 패키지 활용
-// 상태 관리 → React.useState() 활용
-// 사이드 이펙트 관리 → React.useEffect() 활용
-// 참조 관리 → React.useRef() 활용
+// - [x]  컴파운드 컴포넌트 → 함수_컴포넌트.컴포넌트 작성
+// - [x]  props 검사 → PropTypes 패키지 활용
+// - [x]  참조 관리 → React.useRef() 활용
+// - [x]  사이드 이펙트 관리 → React.useEffect() 활용
+// - [x]  함수 메모(memo: 기억) 관리 → React.useCallback() 활용
 
-export class Dialog extends React.Component {
-  // 컴파운드 컴포넌트 (클래스 멤버)
-  static Dim = function DialogDim({ onClose }) {
-    return <div role="presentation" className={styles.dim} onClick={onClose} />;
-  };
+const bodyNode = document.body;
 
-  static Head = function DialogHead({
-    as: Comp = 'header',
-    className = '',
-    ...restProps
-  }) {
-    return (
-      <Comp className={classNames(styles.head, className)} {...restProps} />
-    );
-  };
+export function Dialog({ isVisible, onClose, children, ...restProps }) {
+  // 참조 객체 생성
+  const dialogRef = useRef(null);
+  const opennerButtonRef = useRef(null);
 
-  static Foot = function DialogFoot({
-    as: Comp = 'footer',
-    className = '',
-    ...restProps
-  }) {
-    return (
-      <Comp className={classNames(styles.foot, className)} {...restProps} />
-    );
-  };
+  // 이벤트 리스너
+  const handleClose = useCallback(() => {
+    onClose();
+    opennerButtonRef.current.focus();
+  }, [onClose]);
 
-  static Main = function DialogMain({
-    as: Comp = 'article',
-    className = '',
-    ...restProps
-  }) {
-    return (
-      <Comp className={classNames(styles.main, className)} {...restProps} />
-    );
-  };
+  // 사이드 이펙트 관리
+  useEffect(() => {
+    // 모달 다이얼로그 오프너 버튼 current 값에 참조
+    opennerButtonRef.current = document.activeElement;
 
-  static CloseButton = function DialogCloseButton({
-    onClose = null,
-    ...restProps
-  }) {
-    return (
-      <button
-        type="button"
-        className={styles.closeButton}
-        aria-label="모달 다이얼로그 닫기"
-        title="모달 다이얼로그 닫기"
-        onClick={onClose}
-      >
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          fillRule="evenodd"
-          clipRule="evenodd"
-        >
-          <path d="M12 11.293l10.293-10.293.707.707-10.293 10.293 10.293 10.293-.707.707-10.293-10.293-10.293 10.293-.707-.707 10.293-10.293-10.293-10.293.707-.707 10.293 10.293z" />
-        </svg>
-      </button>
-    );
-  };
-
-  dialogRef = React.createRef(null);
-  opennerButton = null;
-
-  render() {
-    const { isVisible, children } = this.props;
-
-    return createPortal(
-      <section
-        ref={this.dialogRef}
-        role="dialog"
-        aria-modal="true"
-        className={styles.container}
-        aria-hidden={!isVisible}
-        aria-label="React Portal﹕모달 다이얼로그"
-      >
-        {children}
-        <Dialog.CloseButton onClose={this.close} />
-        <Dialog.Dim onClose={this.close} />
-      </section>,
-      document.body
-    );
-  }
-
-  close = () => {
-    this.props.onClose();
-    this.opennerButton.focus();
-  };
-
-  bindKeyEvent() {
-    const tabbableElements = getTabbableElements(this.dialogRef.current);
+    // tabbable 요소들 찾기
+    const tabbableElements = getTabbableElements(dialogRef.current);
     const firstTabbableElement = tabbableElements[0];
     const lastTabbableElement = tabbableElements[tabbableElements.length - 1];
 
+    // 첫번째 탭 이동 가능한 요소에 포커싱
     firstTabbableElement.focus();
 
+    // 이벤트 타입
     let eventType = 'keydown';
 
-    const handleEvent = (e) => {
+    // 이벤트 리스너 정의
+    const eventListener = (e) => {
       const { key, shiftKey, target } = e;
 
       if (
@@ -131,21 +67,115 @@ export class Dialog extends React.Component {
       }
 
       if (key === 'Escape') {
-        this.close();
+        handleClose();
       }
     };
 
-    document.addEventListener(eventType, handleEvent);
-    return () => document.removeEventListener(eventType, handleEvent);
-  }
+    // 이벤트 구독
+    document.addEventListener(eventType, eventListener);
 
-  componentDidMount() {
-    this.opennerButton = document.activeElement;
-    this.unbindKeyEvent = this.bindKeyEvent();
-  }
+    // 이펙트 클린업(정리) 함수
+    return () => {
+      // 이벤트 구독 취소
+      document.removeEventListener(eventType, eventListener);
+    };
+  }, [handleClose]);
 
-  componentWillUnmount() {
-    this.unbindKeyEvent();
-    // this.opennerButton.focus();
-  }
+  return createPortal(
+    <section
+      ref={dialogRef}
+      role="dialog"
+      aria-modal="true"
+      className={styles.container}
+      aria-hidden={!isVisible}
+      aria-label="React Portal﹕모달 다이얼로그"
+      {...restProps}
+    >
+      {children}
+      <Dialog.CloseButton onClose={handleClose} />
+      <Dialog.Dim onClose={handleClose} />
+    </section>,
+    bodyNode
+  );
 }
+
+/* -------------------------------------------------------------------------- */
+
+Dialog.Dim = function DialogDim({ onClose }) {
+  return <div role="presentation" className={styles.dim} onClick={onClose} />;
+};
+Dialog.Dim.propTypes = {
+  onClose: func,
+};
+
+/* -------------------------------------------------------------------------- */
+
+Dialog.Head = function DialogHead({
+  as: Comp = 'header',
+  className = '',
+  ...restProps
+}) {
+  return <Comp className={classNames(styles.head, className)} {...restProps} />;
+};
+Dialog.Head.propTypes = {
+  as: oneOfType([string, elementType]),
+  className: string,
+  restProps: any,
+};
+
+/* -------------------------------------------------------------------------- */
+
+Dialog.Foot = function DialogFoot({
+  as: Comp = 'footer',
+  className = '',
+  ...restProps
+}) {
+  return <Comp className={classNames(styles.foot, className)} {...restProps} />;
+};
+Dialog.Foot.propTypes = {
+  as: oneOfType([string, elementType]),
+  className: string,
+  restProps: any,
+};
+
+/* -------------------------------------------------------------------------- */
+
+Dialog.Main = function DialogMain({
+  as: Comp = 'article',
+  className = '',
+  ...restProps
+}) {
+  return <Comp className={classNames(styles.main, className)} {...restProps} />;
+};
+Dialog.Main.propTypes = {
+  as: oneOfType([string, elementType]),
+  className: string,
+  restProps: any,
+};
+
+/* -------------------------------------------------------------------------- */
+
+Dialog.CloseButton = function DialogCloseButton({ onClose }) {
+  return (
+    <button
+      type="button"
+      className={styles.closeButton}
+      aria-label="모달 다이얼로그 닫기"
+      title="모달 다이얼로그 닫기"
+      onClick={onClose}
+    >
+      <svg
+        width="24"
+        height="24"
+        xmlns="http://www.w3.org/2000/svg"
+        fillRule="evenodd"
+        clipRule="evenodd"
+      >
+        <path d="M12 11.293l10.293-10.293.707.707-10.293 10.293 10.293 10.293-.707.707-10.293-10.293-10.293 10.293-.707-.707 10.293-10.293-10.293-10.293.707-.707 10.293 10.293z" />
+      </svg>
+    </button>
+  );
+};
+Dialog.CloseButton.propTypes = {
+  onClose: func,
+};
