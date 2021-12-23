d59b6d2eaac1a5144dd04de15f774d9dea9c7e62
yamoo9 <yamoog@naver.com>
2021-12-23 15:19:57 +0900

    [학습] useEffect 훅의 클린업 (componentWillUnmount 와의 차이점)

--- container-component-practice/client/src/App.js
+++ container-component-practice/client/src/App.js
@@ -2,5 +2,37 @@ import React from 'react';
 import { TiltCardContainer } from 'containers';
 
 export default function App() {
-  return <TiltCardContainer />;
+  const [visible, setVisible] = React.useState(true);
+
+  React.useEffect(() => {
+    // 이벤트 구독
+    console.log('이벤트 구독');
+    let clearId = null;
+    const timeNode = document.querySelector('time');
+    if (timeNode) {
+      clearId = setInterval(() => {
+        // DOM 스크립트
+        timeNode.textContent = new Date().toLocaleTimeString();
+      }, 1000);
+    }
+
+    // 클린업 함수 (이펙트 함수가 반환하는 함수)
+    return function cleanup() {
+      // 이벤트 구독 취소
+      console.log('이벤트 구독 취소');
+      // 이펙트 함수가 다시 실행되기 전에 정리
+      clearInterval(clearId);
+    };
+  }, [visible]);
+
+  return (
+    <>
+      <TiltCardContainer />
+      <button type="button" onClick={() => setVisible(!visible)}>
+        toggle visible
+      </button>
+      <br />
+      {visible ? <time /> : null}
+    </>
+  );
 }
--- container-component-practice/client/src/hooks/useName.js
+++ container-component-practice/client/src/hooks/useName.js
@@ -1,7 +1,14 @@
 import { useState, useEffect } from 'react';
 
+// this.setState({prop: newPropValue}, () => {
+//   상태 업데이트 후에 콜백 실행 (처리)
+// })
+
 export const useName = (initialName = 'euid') => {
+  // useState() → [상태, 상태 업데이트 함수]
   const [name, setName] = useState('euid');
+
+  // useEffect() → 이펙트 처리 (의존하는 상태가 변경되면 콜백 함수를 실행합니다.)
   useEffect(() => {
     console.group('name 업데이트 된 경우');
     console.log('changed name:', name);
