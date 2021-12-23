70d8ae18ed507ca86994a4e5f663387be9d78605
yamoo9 <yamoog@naver.com>
2021-12-23 13:37:05 +0900

    [학습] 함수 컴포넌트에서의 상태 관리에서 알아두어야 할 점 (상태 합성이 아닌, 대체)

--- container-component-practice/client/src/containers/TiltCardContainer/TiltCardContainer.js
+++ container-component-practice/client/src/containers/TiltCardContainer/TiltCardContainer.js
@@ -1,44 +1,55 @@
+/* eslint-disable */
 import './TiltCardContainer.css';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { TiltCard, A11yHidden } from 'components';
 import { getTiltCard } from 'api';
 
+const initialization = () => {
+  let countValueKey = 'euid-count';
+  let value = localStorage.getItem(countValueKey);
+  if (!value) {
+    localStorage.setItem(countValueKey, JSON.stringify({ count: 1000 }));
+  }
+  return value ? JSON.parse(value).count : 100;
+};
+
 export function TiltCardContainer(props) {
-  // const [cards, setCards] = useState([]);
-  // useEffect(() => getTiltCard().then(({ cards }) => setCards(cards)));
+  // [상태 관리]
+  // 여러 상태를 묶어서 관리하는 방법 추천 X
+  // 관심사의 분리가 되지 않기 때문
+  // 그리고 성능 이슈 문제 유발 (불필요한 리 렌더링 시도)
+  // const [count] = useState(10);
+  // const [name] = useState('euid');
+  const [state, updateState] = useState({
+    a: 1,
+    b: 2,
+    count: 10,
+    name: 'euid',
+  });
 
-  const [times, setTimes] = useState(0);
+  // 상태 관리 (지연된 초기화(함수) 활용)
+  // const [count] = useState(initialization);
 
-  // componentDidMount (1)
-  // componentDidUpdate (N)
-  // componentWillUnmount (1)
-
-  useEffect(
-    /* after mount & update → callback */
-    () => {
-      // 이벤트 구독
-      console.log('이벤트 구독');
-      // 타임아웃 설정
-      let clearId = setTimeout(() => setTimes(times + 10), 1000);
-
-      return () => {
-        // console.log('클린업');
-        console.log('이벤트 구독 취소');
-        // 타임아웃 클리어
-        clearTimeout(clearId);
-      };
-    },
-    /* state dependency array */
-    [times]
-  );
+  // [사이드 이펙트 관리]
 
   return (
-    <div className="tiltCardContainer" lang="en" aria-labelledby="tiltcards">
-      <A11yHidden id="tiltcards">카드 목록 레이블</A11yHidden>
-      <output style={{ display: 'block', margin: 100 }} lang="en">
-        {times} times
-      </output>
-      {/* <div className="tiltCardContainer__buttonGroup">
+    <>
+      <button
+        type="button"
+        style={{ display: 'block' }}
+        onClick={() =>
+          updateState({
+            // ...state,
+            name: 'yamoo9',
+          })
+        }
+      >
+        update name
+      </button>
+      <output>{state.count}</output> <output>{state.name}</output>
+      <div className="tiltCardContainer" lang="en" aria-labelledby="tiltcards">
+        <A11yHidden id="tiltcards">카드 목록 레이블</A11yHidden>
+        {/* <div className="tiltCardContainer__buttonGroup">
         {cards.map(({ id, text }) => (
           <button
             key={id}
@@ -50,7 +61,7 @@ export function TiltCardContainer(props) {
           </button>
         ))}
       </div> */}
-      {/* <ul className="tiltCardContainer__list">
+        {/* <ul className="tiltCardContainer__list">
         {cards.map((card, index) => (
           <li key={card.id}>
             <TiltCard
@@ -62,6 +73,7 @@ export function TiltCardContainer(props) {
           </li>
         ))}
       </ul> */}
-    </div>
+      </div>
+    </>
   );
 }
