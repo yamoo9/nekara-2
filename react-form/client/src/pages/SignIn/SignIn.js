e78879b2f822cddd80b2aa69d13f760833d63baf
yamoo9 <yamoog@naver.com>
2021-12-31 16:52:28 +0900

    [학습] 컴포넌트 메모 - "컴포넌트 또한 값이다."

--- react-form/client/src/pages/SignIn/SignIn.js
+++ react-form/client/src/pages/SignIn/SignIn.js
@@ -1,22 +1,15 @@
 import 'styled-components/macro';
-import { useState, useRef, useEffect } from 'react';
+import { useState, useCallback, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import { FormInput } from 'components';
 
 export function SignIn() {
-  const emailRef = useRef(null);
-  const passwordRef = useRef(null);
   const [email, setEmail] = useState('yamoo9@naver.com');
   const [password, setPassword] = useState('0000');
 
-  useEffect(() => {
-    // console.log('emailRef.current = ', emailRef.current);
-    // console.log('passwordRef.current = ', passwordRef.current);
-  }, []);
-
-  const handleChange = (e) => {
+  // 컴포넌트가 리 렌더링 될 때 마다, 성능 저하를 막기 위해서
+  const handleChange = useCallback((e) => {
     const { name, value } = e.target;
-
     switch (name) {
       default:
       case 'email':
@@ -26,48 +19,65 @@ export function SignIn() {
         setPassword(value);
         break;
     }
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ email, password });
   };
+
+  // 컴포넌트(값) 메모
+  const memoizedEmail = useMemo(
+    () => (
+      <FormInput
+        type="email"
+        id="userMail"
+        label="이메일"
+        inputProps={{
+          autoComplete: 'user-name',
+          name: 'email',
+          value: email,
+          onChange: handleChange,
+        }}
+      >
+        yamoo9@naver.com
+      </FormInput>
+    ),
+    [email, handleChange]
+  );
+
+  const memoizedPassword = useMemo(
+    () => (
+      <FormInput
+        type="password"
+        id="userPass"
+        label="패스워드"
+        inputProps={{
+          autoComplete: 'current-password',
+          name: 'password',
+          value: password,
+          onChange: handleChange,
+        }}
+      >
+        대소문자 조합 6자리 이상 입력하세요.
+      </FormInput>
+    ),
+    [password, handleChange]
+  );
 
   return (
     <>
       <h2>로그인 폼</h2>
       <form
+        onSubmit={handleSubmit}
         css={`
           border: 3px solid currentColor;
           border-radius: 4px;
           padding: 2rem;
         `}
       >
-        <FormInput
-          forwardRef={emailRef}
-          type="email"
-          id="userMail"
-          label="이메일"
-          inputProps={{
-            autoComplete: 'user-name',
-            name: 'email',
-            value: email,
-            onChange: handleChange,
-          }}
-        >
-          yamoo9@naver.com
-        </FormInput>
-
-        <FormInput
-          forwardRef={passwordRef}
-          type="password"
-          id="userPass"
-          label="패스워드"
-          inputProps={{
-            autoComplete: 'current-password',
-            name: 'password',
-            value: password,
-            onChange: handleChange,
-          }}
-        >
-          대소문자 조합 6자리 이상 입력하세요.
-        </FormInput>
-
+        {memoizedEmail}
+        {memoizedPassword}
         <button type="submit">로그인</button>
       </form>
       <p>
